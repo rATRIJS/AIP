19754cba52071b8a7e7d155165e0fd0d2ce80308
Rolands Atvars <rolands.atvars@gmail.com>
2011-09-09 00:00:15 +0100

    Made output prettier.
    Output should now have some formatting attached to it so that it looks better when rendered out in terminal. New class is made to deal with that -> Formatter.

--- lib/Result.php
+++ lib/Result.php
@@ -17,18 +17,70 @@ class Result {
 	public function render() {
 		$render = array();
 		
-		if(!empty($this->php))
-			$render[] = "PHP:\n{$this->php}";
-		
-		if(!empty($this->return))
-			$render[] = "Return:\n{$this->return}";
-		
-		if(!empty($this->output))
-			$render[] = "Output:\n{$this->output}";
+		$this->prepare_php($render);
+		$this->prepare_return($render);
+		$this->prepare_output($render);
+		$this->prepare_message($render);
 			
-		if(!empty($this->message))
-			$render[] = "Message:\n{$this->message}";
-			
-		return implode("\n\n\n", $render);
+		return $this->prepare_render($render);
+	}
+	
+	public function prepare_render($render) {
+		$sep = "\n" . Formatter::load(str_repeat('- ', 25)) . "\n";
+		
+		return "\n" . implode($sep, $render) . "\n";
+	}
+	
+	public function prepare_php(&$render = null) {
+		$php = trim($this->php);
+		if(empty($php)) return;
+		
+		$php = Formatter::load('# PHP:')->bold() . "\n" . $php;
+		
+		if(isset($render)) $render[] = $php;
+		
+		return $php;
+	}
+	
+	public function prepare_return(&$render = null) {
+		$return = null;
+		if(is_scalar($this->return) or is_null($this->return)) {
+			ob_start();
+			var_dump($this->return);
+			$return = ob_get_clean();
+			$return = trim($return);
+		}
+		else {
+			$return = print_r($this->return, true);
+			$return = trim($return);
+		}
+		
+		$return = Formatter::load('# Return')->bold() . "\n" . $return;
+		
+		if(isset($render)) $render[] = $return;
+		
+		return $return;
+	}
+	
+	public function prepare_output(&$render = null) {
+		$output = trim($this->output);
+		if(empty($output)) return;
+		
+		$output = Formatter::load('# Output')->bold() . "\n" . $output;
+		
+		if(isset($render)) $render[] = $output;
+		
+		return $output;
+	}
+	
+	public function prepare_message(&$render = null) {
+		$message = trim($this->message);
+		if(empty($message)) return;
+		
+		$message = Formatter::load($message)->bold();
+		
+		if(isset($render)) $render[] = $message;
+		
+		return $message;
 	}
 }
